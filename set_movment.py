314cefb3e16278b3b33036c30410072812f234b9
aflores614 <andres.qq.flores@gmail.com>
2024-08-09 18:52:39 -0700

    update

--- set_movment.py
+++ set_movment.py
@@ -8,7 +8,7 @@ def fly_movment(master, vx, vy, vz):
     # vz value for down negative value for up 
     master.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, master.target_system,  
                                                                                  master.target_component, 
-                                                                                 mavutil.MAV_FRAME_BODY_OFFSET_NED,
+                                                                                 mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
                                                                                  int(0b110111000111 ), 
                                                                                  0, 0, 0, 
                                                                                  vx, vy , vz, 
